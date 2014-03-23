60d7966055dc6b592b2ec4cba53b2ac87c99aa2c
unknown <Lorenzo@.(none)>
2014-03-23 17:31:22 +0100

    class not found

--- P2.java
+++ P2.java
@@ -1,18 +1,20 @@
+package default
+
 import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
 public class P2 extends HttpServlet {
 
-	  public void doGet(HttpServletRequest request,
-	                    HttpServletResponse response)
-	            throws ServletException, IOException
-	  {
-	      // Set response content type
-	      response.setContentType("text/html");
-
-	      // Actual logic goes here.
-	      PrintWriter out = response.getWriter();
-	      out.println("<h1>" + "helo world" + "</h1>");
-	  }
+	@Override
+	protected void doGet(HttpServletRequest request,
+	                  HttpServletResponse response)
+	          throws ServletException, IOException
+	{
+	    // Set response content type
+	    response.setContentType("text/html");
+	    // Actual logic goes here.
+	    PrintWriter out = response.getWriter();
+    	out.println("<h1>" + "helo world" + "</h1>");
+	}
 }
