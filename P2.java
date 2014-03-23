83ae295d55158f59f10984d45173161b6f207977
unknown <Lorenzo@.(none)>
2014-03-23 22:36:32 +0100

    trying parameters

--- P2.java
+++ P2.java
@@ -27,7 +27,7 @@ public class P2 extends HttpServlet {
     public void doPost(HttpServletRequest request, HttpServletResponse response)
     	    throws IOException, ServletException
     {
-    	phase = request.getParameter("fase");
+    	string phase = request.getParameter("fase");
     	
     	response.setContentType("text/html");
         PrintWriter out = response.getWriter();
