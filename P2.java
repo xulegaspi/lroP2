1438b215b995cc7e5b4390bc1c02bd1616504fd0
unknown <Lorenzo@.(none)>
2014-03-23 18:06:17 +0100

    class not found

--- P2.java
+++ P2.java
@@ -7,6 +7,15 @@ public class P2 extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response)
     throws IOException, ServletException
     {
-
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Hello World!</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1>Hello World!</h1>");
+        out.println("</body>");
+        out.println("</html>");
     }
 }
