90928fdfc1a10cdf721bf4b0faba2d7fa0cb2366
unknown <Lorenzo@.(none)>
2014-03-23 16:42:56 +0100

    Hello world

--- P2.java
+++ P2.java
@@ -3,13 +3,6 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 
 public class P2 extends HttpServlet {
-	private String message;
-
-	  public void init() throws ServletException
-	  {
-	      // Do required initialization
-	      message = "Hello World";
-	  }
 
 	  public void doGet(HttpServletRequest request,
 	                    HttpServletResponse response)
@@ -20,11 +13,6 @@ public class P2 extends HttpServlet {
 
 	      // Actual logic goes here.
 	      PrintWriter out = response.getWriter();
-	      out.println("<h1>" + message + "</h1>");
-	  }
-	  
-	  public void destroy()
-	  {
-	      // do nothing.
+	      out.println("<h1>" + "helo world" + "</h1>");
 	  }
 }
