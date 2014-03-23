0f9107e2c114a79b726cc4db38b3cf167a6687ae
unknown <Lorenzo@.(none)>
2014-03-23 20:23:24 +0100

    trying post

--- P2.java
+++ P2.java
@@ -9,13 +9,23 @@ public class P2 extends HttpServlet {
     {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
-        out.println("<html>");
-        out.println("<head>");
-        out.println("<title>Hello World!</title>");
-        out.println("</head>");
-        out.println("<body>");
-        out.println("<h1>Hello World!</h1>");
-        out.println("</body>");
-        out.println("</html>");
+        out.println("<html><head><title>Servicio de TV</title>");
+        out.println("</head><body>");
+        out.println("<h1>Servicio de consulta de la programaci&oacute;n</h1>");
+        out.println("<h2>Bienvenido a este servicio</h2>");
+        out.println("<h3>Selecciona una consulta:</h3>");
+        out.println("<form method='POST' action='?fase=1'>");
+        out.println("<input type='radio' name='consulta' value='1' checked> Consulta 1<br>");
+       	out.println("<input type='radio' name='consulta' value='2'> Consulta 2<br>");
+        out.println("<p><input type='submit' value='Enviar'>");
+        out.println("</form>");
+        out.println("</body></html>");
+
+    }
+    
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+    	    throws IOException, ServletException
+    {
+    	out.println("<h1>POST</h1>");
     }
 }
