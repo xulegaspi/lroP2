391e0b5595a26431023427d774c178eecaa0adcc
unknown <Lorenzo@.(none)>
2014-03-24 11:01:33 +0100

    step 1 added

--- P2.java
+++ P2.java
@@ -14,10 +14,10 @@ public class P2 extends HttpServlet {
         out.println("</head><body>");
         out.println("<h1>Servicio de consulta de la programaci&oacute;n</h1>");
         out.println("<h2>Bienvenido a este servicio</h2>");
-        out.println("<h3>Selecciona una consulta:</h3>");
-        out.println("<form method='POST' action='?fase=1'>");
-        out.println("<input type='radio' name='consulta' value='1' checked> Consulta 1<br>");
-       	out.println("<input type='radio' name='consulta' value='2'> Consulta 2<br>");
+        out.println("<h3>¿Que quieres ver?</h3>");
+        out.println("<form method='POST' action='?step=1'>");
+        out.println("<input type='radio' name='query' value='series' checked> Consulta Series<br>");
+       	out.println("<input type='radio' name='query' value='films'> Consulta Pel&iacute;culas<br>");
         out.println("<p><input type='submit' value='Enviar'>");
         out.println("</form>");
         out.println("</body></html>");
@@ -27,10 +27,53 @@ public class P2 extends HttpServlet {
     public void doPost(HttpServletRequest request, HttpServletResponse response)
     	    throws IOException, ServletException
     {
-    	String phase = request.getParameter("fase");
-    	
+    	String step = request.getParameter("step");
     	response.setContentType("text/html");
         PrintWriter out = response.getWriter();
+    	
+    	if(step.equals("1")){
+    		String query = request.getParameter("query");
+    		if(query.equals("series")){
+    			out.println("<html><head><title>Servicio TV</title>");
+    		    out.println("</head><body>");
+    		    out.println("<h1>Servicio de consulta de la programaci&oacute;n</h1>");
+    		    out.println("<h2>Series</h2>");
+    		    out.println("<h3>Selecciona un d&iacute;a:</h3>");
+    		    out.println("<form method='POST' action='?step=2'>");
+    		    out.println("<input type='radio' name='dia' value='01/12/2013' checked> 01/12/2013<BR>");
+    		    out.println("<input type='radio' name='dia' value='02/12/2013' checked> 02/12/2013<BR>");
+    		    out.println("<input type='radio' name='dia' value='03/12/2013' checked> 03/12/2013<BR>");
+    		    out.println("<input type='radio' name='dia' value='04/12/2013' checked> 04/12/2013<BR>");
+    		    out.println("<input type='radio' name='dia' value='05/12/2013' checked> 05/12/2013<BR>");
+    		    out.println("<p><input type='submit' value='Enviar'>");
+    		    out.println("<input type='submit' value='Atr&aacute;s' onClick='document.forms[0].method=\"GET\"'>");
+    		    out.println("</form>");
+    		    out.println("</body></html>");
+    		}
+    		else if(query.equals("films")){
+    			out.println("<html><head><title>Servicio TV</title>");
+   		     	out.println("</head><body>");
+   		     	out.println("<h1>Servicio de consulta de la programaci&oacute;n</h1>");
+   		     	out.println("<h2>Pel&iacute;culas</h2>");
+   		     	out.println("<h3>Selecciona un idioma:</h3>");
+   		     	out.println("<form method='POST' action='?step=2'>");
+   		     	out.println("<input type='radio' name='idioma' value='en' checked> en<BR>");
+   		     	out.println("<input type='radio' name='idioma' value='fr' checked> fr<BR>");
+   		     	out.println("<input type='radio' name='idioma' value='it' checked> it<BR>");
+   		     	out.println("<input type='radio' name='idioma' value='de' checked> de<BR>");
+   		     	out.println("<input type='radio' name='idioma' value='es' checked> es<BR>");
+   		     	out.println("<p><input type='submit' value='Enviar'>");
+   		     	out.println("<input type='submit' value='Atr&aacute;s' onClick='document.forms[0].method=\"GET\"'>");
+   		     	out.println("</form>");
+   		     	out.println("</body></html>");
+    		}
+    		else {
+    			//do 404
+    		}
+    	}
+    	else if(step.equals(2)){
+    		
+    	}
         
     	out.println("<h1>" + phase + "</h1>");
     }
