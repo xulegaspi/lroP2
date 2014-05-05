9f6f686b5b58e2f3321c7affe0f93c652cabe3d2
xulegaspi <xulegaspi@gmail.com>
2014-05-05 17:38:32 +0200

    end2

--- TvmlReader.java
+++ TvmlReader.java
@@ -6,7 +6,6 @@ import java.util.ListIterator;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.DocumentBuilder;
 
-import com.sun.java.browser.plugin2.DOM;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
