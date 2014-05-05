d345322e637adcd92554b33b8fcaf3c5b5488eed
xulegaspi <xulegaspi@gmail.com>
2014-05-05 03:52:06 +0200

    tvml.xml added but not changed.

--- TvmlReader.java
+++ TvmlReader.java
@@ -22,12 +22,12 @@ public class TvmlReader {
 
     void TvmlReader(){}
 
-    private void addLang(String langs){
+    private void addLang(String langs) {
         String[] langl = langs.split("\\ ");
-        for(int ii=0; ii<langl.length; ii++){
+        for(int ii=0; ii<langl.length; ii++) {
             ListIterator<String> it = langsList.listIterator();
             boolean included = false;
-            for(int jj=0; jj<langsList.size(); jj++){
+            for(int jj=0; jj<langsList.size(); jj++) {
                 if(it.next().equals(langl[ii])) {
                     included = true;
                 }
@@ -36,8 +36,8 @@ public class TvmlReader {
         }
     }
 
-    String Read(){
-        try{
+    String Read() {
+        try {
             DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
             dbf.setValidating(true);
             DocumentBuilder db = dbf.newDocumentBuilder();
@@ -46,12 +46,12 @@ public class TvmlReader {
             langsList = new ArrayList<String>();
             daysList = new ArrayList<String>();
 
-            Document doc = db.parse("http://localhost:8024/lro24/tvml-ok.xml");
+            Document doc = db.parse("http://localhost:8022/lro22/tvml-ok.xml");
             DOMList.add(doc);
 
             ListIterator<Document> it = DOMList.listIterator();
             int ii=0;
-            do{
+            do {
                 it = DOMList.listIterator(ii);
                 doc = it.next();
                 NodeList lChannels = doc.getElementsByTagName("Canal");
@@ -71,22 +71,22 @@ public class TvmlReader {
 
                     // look for more tvmls
                     NodeList nlUrl = eChannel.getElementsByTagName("UrlTVML");
-                    if(nlUrl.getLength()>0){
+                    if(nlUrl.getLength()>0) {
                         url=nlUrl.item(0).getTextContent();
-                        try{
+                        try {
                             doc = db.parse(url);
                             DOMList.add(doc);
-                        }catch(Exception ex){
+                        } catch(Exception ex) {
                             ex.printStackTrace();
                             url="no doc found";
                         }
                     }
                 }
                 ii++;
-            }while(ii<DOMList.size());
+            } while(ii<DOMList.size());
             return "readed" + ii;
 
-        }catch(Exception ex){
+        } catch(Exception ex) {
             //ex.printStackTrace();
             final StringWriter sw = new StringWriter();
             final PrintWriter pw = new PrintWriter(sw, true);
@@ -95,28 +95,28 @@ public class TvmlReader {
         }
     }
 
-    List<String> getDays(){
+    List<String> getDays() {
         return daysList;
     }
 
-    List<String> getLanguages(){
+    List<String> getLanguages() {
         return langsList;
     }
 
-    List<String> getChannels(String day){
+    List<String> getChannels(String day) {
         return this.getChannels(day, "all");
     }
 
-    List<String> getChannels(String day, String lang){
+    List<String> getChannels(String day, String lang) {
         List<String> channelList = new ArrayList<String>();
         ListIterator<String> it = daysList.listIterator();
-        for(int ii=0; ii<daysList.size(); ii++){
+        for(int ii=0; ii<daysList.size(); ii++) {
             if(it.next().equals(day)) {
                 ListIterator<Document> docIt = DOMList.listIterator(ii);
                 NodeList lChannels = docIt.next().getElementsByTagName("Canal");
-                for(int jj=0; jj<lChannels.getLength(); jj++){
+                for(int jj=0; jj<lChannels.getLength(); jj++) {
                     Element eChannel = (Element)lChannels.item(jj);
-                    if(eChannel.getAttribute("lang").equals(lang) || lang.equals("all")){
+                    if(eChannel.getAttribute("lang").equals(lang) || lang.equals("all")) {
                         channelList.add(eChannel.getElementsByTagName("NombreCanal").item(0).getTextContent());
                     }
                 }
@@ -127,22 +127,22 @@ public class TvmlReader {
         return channelList;
     }
 
-    List<FilmPkg> getFilms(String day, String channel){
+    List<FilmPkg> getFilms(String day, String channel) {
         List<FilmPkg> filmList = new ArrayList<FilmPkg>();
         ListIterator<String> it = daysList.listIterator();
-        for(int ii=0; ii<daysList.size(); ii++){
+        for(int ii=0; ii<daysList.size(); ii++) {
             if(it.next().equals(day)) {
                 ListIterator<Document> docIt = DOMList.listIterator(ii);
                 NodeList lChannels = docIt.next().getElementsByTagName("Canal");
-                for(int jj=0; jj<lChannels.getLength(); jj++){
+                for(int jj=0; jj<lChannels.getLength(); jj++) {
                     Element eChannel = (Element)lChannels.item(jj);
                     String sChannel = eChannel.getElementsByTagName("NombreCanal").item(0).getTextContent();
-                    if(sChannel.equals(channel) || channel.equals("all")){
+                    if(sChannel.equals(channel) || channel.equals("all")) {
                         NodeList lPrograms = eChannel.getElementsByTagName("Programa");
-                        for(int ij=0; ij<lPrograms.getLength(); ij++){
+                        for(int ij=0; ij<lPrograms.getLength(); ij++) {
                             Element eFilm = (Element)lPrograms.item(ij);
                             String category = eFilm.getElementsByTagName("Categoria").item(0).getTextContent();
-                            if(category.equals("Cine")){
+                            if(category.equals("Cine")) {
                                 FilmPkg film = new FilmPkg();
                                 film.title = eFilm.getElementsByTagName("NombrePrograma").item(0).getTextContent();
                                 Element eIntervalo = (Element)eFilm.getElementsByTagName("Intervalo").item(0);
@@ -165,20 +165,20 @@ public class TvmlReader {
         return filmList;
     }
 
-    List<ShowPkg> getShows(String day, String channel, String lang){
+    List<ShowPkg> getShows(String day, String channel, String lang) {
         List<ShowPkg> showList = new ArrayList<ShowPkg>();
         ListIterator<String> it = daysList.listIterator();
-        for(int ii=0; ii<daysList.size(); ii++){
+        for(int ii=0; ii<daysList.size(); ii++) {
             if(it.next().equals(day)) {
                 ListIterator<Document> docIt = DOMList.listIterator(ii);
                 NodeList lChannels = docIt.next().getElementsByTagName("Canal");
-                for(int jj=0; jj<lChannels.getLength(); jj++){
+                for(int jj=0; jj<lChannels.getLength(); jj++) {
                     Element eChannel = (Element)lChannels.item(jj);
                     String sChannel = eChannel.getElementsByTagName("NombreCanal").item(0).getTextContent();
                     if((sChannel.equals(channel) || channel.equals("all")) &&
-                            (eChannel.getAttribute("lang").equals(lang) || lang.equals("all") )){
+                            (eChannel.getAttribute("lang").equals(lang) || lang.equals("all") )) {
                         NodeList lPrograms = eChannel.getElementsByTagName("Programa");
-                        for(int ij=0; ij<lPrograms.getLength(); ij++){
+                        for(int ij=0; ij<lPrograms.getLength(); ij++) {
                             Element eShow = (Element)lPrograms.item(ij);
                             ShowPkg show = new ShowPkg();
                             show.name = eShow.getElementsByTagName("NombrePrograma").item(0).getTextContent();
